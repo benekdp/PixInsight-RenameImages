eeec0c89ce9aa0cfe839751d62ed48a5fb3cb18f
benekdp <benekaus@gmail.com>
2024-07-15 10:37:24 +0200

    Add files via upload

---
+++ pixinsight-updates/src/filter_settings.js
@@ -0,0 +1,9 @@
+var filterSettings = {
+    LUMINANCE: "Luminance",
+    RED: "Red",
+    GREEN: "Green",
+    BLUE: "Blue",
+    SULFUR: "Sulfur",
+    HYDROGEN: "HydrogenAlpha",
+    OXYGEN: "Oxygen"
+};
